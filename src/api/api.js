123e4c049e2cac0c42550260f5b83c64895187b8
Laudierst <laudiersantanamei@gmail.com>
2024-09-18 00:52:12 -0300

    Nova atualização no arquivo index.ts

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "https://apinonshops.store"
+    baseURL: "http://35.193.184.30"
+    //baseURL: "https://apinonshops.store"
     //baseURL: "https://api-store-stylestop.onrender.com" 
     //"http://localhost:3003" 
     //"https://api-store-v4bm.onrender.com"
